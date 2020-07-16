03aeee7ea389c5ef4405a9b9d859c85cbc3db4b0
Claudia Layrisse <clayrisse@ClaudiaLayrisse.localdomain>
2020-07-16 16:50:52 +0200

    coinbat page works and links check

--- js/FuckOffServiceApi.js
+++ js/FuckOffServiceApi.js
@@ -1,9 +1,26 @@
 'use strict'
 
 
+const players = {
+  johnny: { name: 'Johnny Cage', state: true, imgpath:'assets/img/p-johnny.jpg' },
+  subzero: { name: 'Sub-Zero', state: true, imgpath:"assets/img/p-subzero.jpg" },
+  sonya: { name: 'Sonya Blade', state: true, imgpath:"assets/img/p-sonya.jpg" },
+  raiden: { name: 'Raiden', state: true, imgpath:"assets/img/p-raiden.jpg" },
+  liu: { name: 'Liu Kang', state: true, imgpath:"assets/img/p-liu.jpg" },
+  scorpio: { name: 'Scorpio', state: true, imgpath:"assets/img/p-scorpio.jpg" },
+  kano: { name: 'Kano', state: true, imgpath:"assets/img/p-kano.jpg" },
+  kitana: { name: 'Kitana', state: true, imgpath:"assets/img/p-kitana.jpg" },
+}
+// let winMessage1 = `Poin for ${play1}`
+// let winMessage2 = `Poin for ${play2}`
+// let flawlessVictoryMsg = "Flawless Victory" 
 
-// Localizador del mensaje de array
-// const pickedMessages = [0, 3, 32, 33, 39, 59, 70, 75, 79, 82] //55 is out
+
+const urlParams = new URLSearchParams(window.location.search);
+const winner = urlParams.get('winner');
+const flawlessV = urlParams.get('flawlessV');
+
+
 
 function fuckOff () {
 
@@ -18,7 +35,7 @@ function fuckOff () {
     })
     .then((data) => {
       console.log(data)
-      // idnumber of the messages I personally selected to be shown
+      // id number of the messages I personally selected to be shown
       const pickedMessages = [0, 3, 32, 33, 39, 59, 70, 75, 79, 82] //55 is out
       // I do a random operation to pick one of the 10 messages and look specifically for that one
       let i = Math.floor(Math.random()*10);
@@ -35,3 +52,14 @@ function fuckOff () {
 
 fuckOff ();
 
+
+
+
+
+
+function pickedPlayer2 (player2) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const player1 = urlParams.get('player1');
+  
+  setTimeout( ()=> location.assign(`coinbat.html?player1=${player1}&player2=${player2}`), 500);
+}
--- js/coinbat.js
+++ js/coinbat.js
@@ -1,58 +1,27 @@
 'use strict'
 
 
-// pickPlayer1 page
-//   //*shows menu of players to pick PLAYER1  and with 'click' go to next page directly
-
-// pickPlayer2 page
-//   //*shows menu of players to pick PLAYER2  and with 'click' go to FIGHT page directly
-
-// coinbat page (coin rule=> the winner out 3)
-//     //*shows the 2 players avatar, "life" count, name and a "coin",  -----> CSS
-//     // pasar en la url los parametros player 1 y player 2
-
-//   1st click on coin(btn) ==> does js random()odd/even and 
- 
-//     //resets page with a graphic added to "health level" of the looser 
-//     // contador de wins
-//   2nd click on coin()
-//       //if the looser from 1st clicks wins,           
-//           //resets page with a graphic added to "health level" of the looser (now with both player with 1 hit)
-//       //if the winner from 1st clicks wins, ====> execute 3rd click 
-         
-//   3rd click on coin()
-//           //shows message on whose the winner (3seg last)
-//           //go to FUCK OFF page and show a fucking message.
-
-
 const players = {
   johnny: { name: 'Johnny Cage', state: true, imgpath:'assets/img/p-johnny.jpg' },
   subzero: { name: 'Sub-Zero', state: true, imgpath:"assets/img/p-subzero.jpg" },
   sonya: { name: 'Sonya Blade', state: true, imgpath:"assets/img/p-sonya.jpg" },
   raiden: { name: 'Raiden', state: true, imgpath:"assets/img/p-raiden.jpg" },
   liu: { name: 'Liu Kang', state: true, imgpath:"assets/img/p-liu.jpg" },
-  Scorpio: { name: 'Scorpio', state: true, imgpath:"assets/img/p-scorpio.jpg" },
+  scorpio: { name: 'Scorpio', state: true, imgpath:"assets/img/p-scorpio.jpg" },
   kano: { name: 'Kano', state: true, imgpath:"assets/img/p-kano.jpg" },
   kitana: { name: 'Kitana', state: true, imgpath:"assets/img/p-kitana.jpg" },
 }
-// let winMessage1 = `Poin for ${play1}`
-// let winMessage2 = `Poin for ${play2}`
-// let flawlessVictoryMsg = "Flawless Victory" 
 
+//----------------no borrar esto nunca!
+let coinFlipObj;
+//------------------------------------
 
-
-// function renderSetUp() {
-//   renderImageP1();
-//   renderImageP2();
-//   renderNameP1();
-//   renderNameP2();
-// }
 
 function renderPage(player1, player2) {
   let oldMainGone =  document.querySelector('main')
   document.body.removeChild(oldMainGone);
   const main = document.createElement('main');
- console.log(players)
+//  console.log(players)
   main.innerHTML = `          
   <section id="playboard" class="board">           
   <!-- ------------------player 1--------------------------- -->     
@@ -100,9 +69,6 @@ function renderPage(player1, player2) {
   <img class="battleplayer-logo" src="assets/img/logoletters-mortalcoinbat.png" alt="player">  
 </div>
   `;
-
-
- 
   document.body.appendChild(main);
 
 }
@@ -117,51 +83,48 @@ function coinFlip() {
   let i = Math.floor(Math.random()*10);
  
   counter += 1
-  // console.log(`flip no${counter} random es ${i}`)
   
   if (i % 2 === 0){ 
     winPlayer2 += 1
     console.log(`flip no${counter} random es ${i} / gana p2 winCount2:${winPlayer2}`);
-    renderHit1()
+    renderHit2()
     winnerCheck()
   } else {
     winPlayer1 += 1
     console.log(`flip no${counter} random es ${i} / gana p1 winCount1:${winPlayer1}`);
-    renderHit2()
+    renderHit1()
     winnerCheck()
   }
 }
 
 function renderHit1() {
   let lifeBar2 =  document.querySelector('#battle-player2-wincount'); 
-  lifeBar2.style.width="33%"//pq esta mostrando esto
 
   switch (winPlayer1) {
     case 1:
-    lifeBar2.style.width="33%";
-    break;
-    case 2:
     lifeBar2.style.width="66%";
     break;
+    case 2:
+    lifeBar2.style.width="33%";
+    break;
     case 3:
-    lifeBar2.style.width="95%";
+    lifeBar2.style.width="1%";
     break;
   } 
 }
 
 function renderHit2() {
   let lifeBar1 =  document.querySelector('#battle-player1-wincount'); 
-  lifeBar1.style.width="33%"//pq esta mostrando esto
   
   switch (winPlayer2) {
     case 1:
-    lifeBar1.style.width="33%";
-    break;
-    case 2:
     lifeBar1.style.width="66%";
     break;
+    case 2:
+    lifeBar1.style.width="33%";
+    break;
     case 3:
-    lifeBar1.style.width="95%";
+    lifeBar1.style.width="1%";
     break;
   } 
 }
@@ -175,43 +138,45 @@ function winnerCheck() {
   } else if (winPlayer1 == 3 && winPlayer2 == 0 ) {
     console.log(`FlawlessVictory P2 winCount:${winPlayer2}`);
     coinFlipObj.removeEventListener('click', coinFlip);
-    // renderFlawlessVictory()
     // renderVictoryPlayer1()
+    setTimeout( ()=> location.assign(`fumessage.html?loser=${player1}&flawlessV=${true}`), 800);
 
     // ----------------------------clean win player 2
   } else if (winPlayer2 == 3 && winPlayer1 == 0) {
     console.log(`FlawlessVictory P1 winCount:${winPlayer1}`);
     coinFlipObj.removeEventListener('click', coinFlip);
-    // renderFlawlessVictory()
     // renderVictoryPlayer2() 
+    setTimeout( ()=> location.assign(`fumessage.html?loser=${player2}&flawlessV=${true}`), 800);
 
     // ----------------------------dirty win player 1
   } else if (winPlayer1 == 3) {
     console.log("gana 1");
     coinFlipObj.removeEventListener('click', coinFlip);
-    //removeenetlistener
     // renderVictoryPlayer1()       
+
+    setTimeout( ()=> location.assign(`fumessage.html?loser=${player1}&flawlessV=${false}`), 800);
 
     // ----------------------------dirty win player 2
   } else if (winPlayer2 == 3) {
     console.log("gana 2")
     coinFlipObj.removeEventListener('click', coinFlip);
     // renderVictoryPlayer2()   
+    setTimeout( ()=> location.assign(`fumessage.html?loser=${player2}&flawlessV=${false}`), 800);
   } 
 } 
 
 
 function messageWinHitPlayer1() {
- console.log( winMessage1) //dislay this
-}
-
-function messageWinHitPlayer2(){
-  // winMessage2 //dislay this
-}
-
-function renderFlawlessVictory() {
+  //salto a pagina mensaje
+ console.log(winMessage1, winPlayer2)
+  if (winPlayer2 == 0){
+  renderFlawlessVictory//hacer codigo
+  } 
+  //render 
 
 }
+
+
 
 
 
@@ -224,8 +189,7 @@ const player2 = urlParams.get('player2');
  
 window.addEventListener('load', () => {
   renderPage(player1, player2);
-  const coinFlipObj = document.querySelector('#thecoin');
+  coinFlipObj = document.querySelector('#thecoin');
   coinFlipObj.addEventListener('click', coinFlip);
-
 
 });
